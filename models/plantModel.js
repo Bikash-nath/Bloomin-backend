0bce611565cff7bc2c16870a7c17a092e83909e4
Spartan-21 <bikash625sf@gmail.com>
2022-09-22 14:31:40 +0530

    Inital commit

--- models/plantModel.js
+++ models/plantModel.js
@@ -10,33 +10,35 @@ const plantSchema = new mongoose.Schema({
     maxlength: [80, 'A plant title must have less or equal then 30 characters'],
   },
   slug: String,
-  light: {
-    type: Number,
-    required: [true, 'A plant must have light required'],
-  },
-  temperature: Number,
-  water: Number,
-  fertilize: Number,
-  botanicalName: String,
-  family: String,
-  type: String,
-  category: String,
-  size: Number,
-  repotting: Number,
-  bloom: {
-    type: Boolean,
-    default: false,
-  },
-  nativeArea: String,
-  imageCover: {
-    type: String,
-    required: [true, 'A plant must have a cover image'],
-  },
-  images: [String],
   description: {
     type: String,
     trim: true,
+    required: [true, 'A plant must have a description'],
   },
+  botanicalName: String,
+  family: String,
+  plantType: String,
+  category: {
+    type: String,
+    trim: true,
+    enum: {
+      values: ['Flowering', 'Foliage', 'Herbs', 'Shrubs', 'Succulents & Cacti', 'Trees'],
+    },
+  },
+  light: String,
+  water: String,
+  temperature: String,
+  fertilize: String,
+  humidity: String,
+  matureSize: String,
+  repotting: String,
+  bloomTime: String,
+  nativeArea: String,
+  imageCover: {
+    type: String,
+    // required: [true, 'A plant must have a cover image'],
+  },
+  images: [String],
 });
 
 const Plant = mongoose.model('plants', plantSchema);
